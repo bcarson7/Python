a11bf3c7f80258fddeb1b0438aac9f9f1a653e08
bcarson7 <bpc31@msn.com>
2017-04-24 22:20:43 -0700

    Create race.py

---
+++ race.py
@@ -0,0 +1,12 @@
+import random
+import math
+
+def race():
+    for i in range(5):
+        print ("{0:.3f}".format(random.uniform(100, 150)))
+
+print('Budweiser')
+race()
+print('')
+print('Tosti Asti')
+race()
