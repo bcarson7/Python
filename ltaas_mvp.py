1d9f3a17708f2bb1cd59f7eccb6f1f22998e4bac
bcarson7 <bpc31@msn.com>
2017-05-12 13:56:00 -0700

    Update ltaas_mvp.py

--- ltaas_mvp.py
+++ ltaas_mvp.py
@@ -1,27 +1,50 @@
 #!/bin/python
+import os
 VU = ''
 URL = ''
+TESTTIME = ''
+TESTNAME = ''
+RAMPTIME = ''
+HOLDTIME = ''
 
-while not URL:
-        print('Enter your load destination including http')
-        URL = raw_input()
+os.system('clear')
 
-print('Emter the number of virtual users? (100 or fewer)')
+print('Enter the test name')
+TESTNAME = raw_input()
+
+print('Enter the load destination including http')
+URL = raw_input()
+
+print('Emter the number of virtual users (100 or fewer)')
 VU = raw_input()
 if int(VU) > 100:
-        VU = 1
+        VU = 100
+
+print('Enter the linear ramp time in minutes')
+RAMPTIME = int(raw_input())
+
+print('Enter the total test time in minutes')
+TESTTIME = int(raw_input())
+
+HOLDTIME = TESTTIME - RAMPTIME
+
 VU=str(VU)
-F = open('test.yml','w')
+RAMPTIME=str(RAMPTIME)
+TESTTIME=str(TESTTIME)
+HOLDTIME=str(HOLDTIME)
+
+F = open(TESTNAME + '.yml','w')
 F.write(
 "execution:" +"\n"
 "- concurrency: " + VU + "\n"
-"  ramp-up: 1m" + "\n"
-"  hold-for: 5m" + "\n"
-"  scenario: quick-test" + "\n"
+"  ramp-up: " + RAMPTIME + "m" + "\n"
+"  hold-for: " + HOLDTIME + "m" + "\n"
+"  scenario: " + TESTNAME + "\n"
 "\n"
 "scenarios:" + "\n"
-"  quick-test:" + "\n"
+" " + TESTNAME + ":" + "\n"
 "    requests:" + "\n"
 "    - " + URL + "\n"
 )
 F.close()
+os.system('bzt ' + TESTNAME + '.yml')
