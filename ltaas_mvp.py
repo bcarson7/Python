2093ca13423dd87953f47341e49cac3f5aff7c72
bcarson7 <bpc31@msn.com>
2017-05-25 11:12:20 -0700

    Update ltaas_mvp.py

--- ltaas_mvp.py
+++ ltaas_mvp.py
@@ -1,16 +1,20 @@
 #!/bin/python
 import os
+
 VU = ''
 URL = ''
 TESTTIME = ''
 TESTNAME = ''
 RAMPTIME = ''
 HOLDTIME = ''
-
+TOKEN = ''
 os.system('clear')
 
 print('Enter the test name')
 TESTNAME = raw_input()
+
+print('Enter Blazemeter Token')
+TOKEN = raw_input()
 
 print('Enter the load destination including http')
 URL = raw_input()
@@ -19,6 +23,7 @@ print('Emter the number of virtual users (100 or fewer)')
 VU = raw_input()
 if int(VU) > 100:
         VU = 100
+#VU=str(VU)
 
 print('Enter the linear ramp time in minutes')
 RAMPTIME = int(raw_input())
@@ -35,6 +40,15 @@ HOLDTIME=str(HOLDTIME)
 
 F = open(TESTNAME + '.yml','w')
 F.write(
+"---" + "\n"
+"modules:" + "\n"
+"  blazemeter:" + "\n"
+"    token: " + TOKEN + "\n"
+"    browser-open: false" + "\n"
+"" + "\n"
+"reporting:" + "\n"
+"  - module: blazemeter" + "\n"
+"    test: " + TESTNAME + "\n"
 "execution:" +"\n"
 "- concurrency: " + VU + "\n"
 "  ramp-up: " + RAMPTIME + "m" + "\n"
