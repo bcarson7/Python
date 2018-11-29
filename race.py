9dbdcc6c9f838fb7d22b6bad28eccaa0d63bfe9c
bcarson7 <bpc31@msn.com>
2018-11-29 16:39:04 -0700

    Update race.py

--- race.py
+++ race.py
@@ -1,20 +1,20 @@
 import random
 
-first_lap = 0
+user = input('Enter the the number of laps')
+user = int(user)
 
 def race(laps):
     total = 0
-    race = 0
     for i in range(laps):
-        first_lap = round(random.uniform(100, 150),3)
-        total += first_lap
-        race = round((total/laps),3)
-        print(first_lap)
-    print("Race Speed: " + str(race))
+        race_lap = round(random.uniform(100, 150),3)
+        total += race_lap
+        heat = round((total/laps), 3)
+        print(race_lap)
+    print("AVG: " + str(heat) + " MPH")
 
 print('U-1')
-race(3)
+race(user)
 
 print('')
 print('U-9')
-race(3)
+race(user)
