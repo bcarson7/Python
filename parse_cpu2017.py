fe8d3de59ece0dc111f13a61435539e0526e3999
bcarson7 <bpc31@msn.com>
2019-03-21 14:16:01 -0700

    Update parse_cpu2017.py

--- parse_cpu2017.py
+++ parse_cpu2017.py
@@ -7,34 +7,28 @@ import os.path
 import csv
 import datetime
 
-int1 = open("/home/tester/test-bench/cpu2017/result/CPU2017.001.intrate.refrate.csv", 'r')
-for test1 in int1:
-    if 'SPECrate2017_int_base' in test1:
-        newCSV = open("speccpu.csv", "w")
-        newCSV.write(test1)
+int_files = ["CPU2017.001.intrate.refrate.csv", "CPU2017.003.intrate.refrate.csv", "CPU2017.005.intrate.refrate.csv"]
+fp_files = ["CPU2017.002.fprate.refrate.csv", "CPU2017.004.fprate.refrate.csv", "CPU2017.006.fprate.refrate.csv"]
 
-int2 = open("/home/tester/test-bench/cpu2017/result/CPU2017.003.intrate.refrate.csv", 'r')
-for test2 in int2:
-    if 'SPECrate2017_int_base' in test2:
-        newCSV.write(test2)
+def open_int(x):
+    cpufile = open("/home/tester/test-bench/cpu2017/" + int_files[x])
+    for line in cpufile:
+        if 'SPECrate2017_int_base' in line:
+            newCSV = open("speccpu.csv", "a")
+            newCSV.write(line)
+            #newCSV.close()
 
-int3 = open("/home/tester/test-bench/cpu2017/result/CPU2017.005.intrate.refrate.csv", 'r')
-for test3 in int3:
-    if 'SPECrate2017_int_base' in test3:
-        newCSV.write(test3)
+def open_fp(x):
+    cpufile = open("/home/tester/test-bench/cpu2017/" + fp_files[x])
+    for line in cpufile:
+        if 'SPECrate2017_fp_base' in line:
+            newCSV = open("speccpu.csv", "a")
+            newCSV.write(line)
+            #newCSV.close()
 
-fp1 = open("/home/tester/test-bench/cpu2017/result/CPU2017.002.fprate.refrate.csv", 'r')
-for fptest1 in fp1:
-    if 'SPECrate2017_fp_base' in fptest1:
-        newCSV.write(fptest1)
-
-fp2 = open("/home/tester/test-bench/cpu2017/result/CPU2017.004.fprate.refrate.csv", 'r')
-for fptest2 in fp2:
-    if 'SPECrate2017_fp_base' in fptest2:
-        newCSV.write(fptest2)
-
-fp3 = open("/home/tester/test-bench/cpu2017/result/CPU2017.006.fprate.refrate.csv", 'r')
-for fptest3 in fp3:
-    if 'SPECrate2017_fp_base' in fptest3:
-        newCSV.write(fptest3)
-        newCSV.close()
+open_int(0)
+open_int(1)
+open_int(2)
+open_fp(0)
+open_fp(1)
+open_fp(2)
